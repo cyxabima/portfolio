8d82015998d417320cfdad7ed8dcbebf985d1d6e
cyxabima <ukashaanwerali@gmail.com>
2025-07-23 22:31:04 +0500

    mdx-content component updated

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -1,25 +1,33 @@
-import { JSX } from 'react'
-import { highlight } from 'sugar-high'
-import Counter from '@/components/counter'
-import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote-client/rsc'
+import { JSX } from 'react';
+import { highlight } from 'sugar-high';
+import Counter from '@/components/counter';
+import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote-client/rsc';
+import React from 'react';
+import Image from 'next/image';
 
-function Code({ children, ...props }: any) {
-    let codeHTML = highlight(children)
-    return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
+// Explicitly type props
+type CodeProps = React.HTMLAttributes<HTMLElement> & {
+    children: string;
+};
+
+function Code({ children, ...props }: CodeProps) {
+    const codeHTML = highlight(children);
+    return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
 }
 
+// Components mapping
 const components = {
     code: Code,
-    Counter
-}
+    Counter,
+    Image
+};
 
-export default function MDXContent(
-    props: JSX.IntrinsicAttributes & MDXRemoteProps
-) {
+// MDX Content renderer
+export default function MDXContent(props: JSX.IntrinsicAttributes & MDXRemoteProps) {
     return (
         <MDXRemote
             {...props}
             components={{ ...components, ...(props.components || {}) }}
         />
-    )
+    );
 }
