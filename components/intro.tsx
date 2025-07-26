6d0c4d53c68f663dd803bb7a24bf9faf2a7b48fc
Ukasha Anwer Ali <ukashaanwerali@gmail.com>
2025-07-26 22:06:57 +0500

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,9 +5,9 @@ export default function Intro() {
     return (
         <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
             <div className='mt-2 flex-1 md:mt-0'>
-                <h1 className='title no-underline'>Hey, I&#39;m Ukasha.</h1>
+                <h1 className='title no-underline'>Hey, I&#39;m Ukasha Anwer.</h1>
                 <p className='mt-3 font-light text-muted-foreground'>
-                    I&#39;m a software engineer based in Karachi, Pakistan. I&#39;m
+                    I&#39;m a computer systems and software engineering student based in Karachi, Pakistan. I&#39;m
                     passionate about learning new technologies and sharing knowledge with
                     others.
                 </p>
